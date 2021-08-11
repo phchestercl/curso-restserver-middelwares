ee5eeb0e6cc0cc7bd7db1ea4ef9734319e96eb16
phchestercl <phchester@gmail.com>
2021-08-11 08:07:35 -0400

    primer console log

--- middleware/validarjwt.js
+++ middleware/validarjwt.js
@@ -34,7 +34,7 @@ const validarJWT= async (req=request, res=response, next)=>{
         
         next();
     } catch (error) {
-        /* console.log(error); */
+        console.log(error);
         res.status(401).json({
             msg:'Token no válido'
         });
